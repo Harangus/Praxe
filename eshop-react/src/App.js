e97f44855d18a2c54054c14feada9e7413c44386
Harangus <133635113+Harangus@users.noreply.github.com>
2023-05-18 11:12:41 +0200

    Add files via upload

--- eshop-react/src/App.js
+++ eshop-react/src/App.js
@@ -5,20 +5,30 @@ import './scss/index.scss';
 import Header from "./header"
 import Navbar from './navbar';
 import ProductRow from './productsRow';
-import Navigation from "./navigation"
+import Navigation from "./navigation";
+import {faker} from "@faker-js/faker";
+
 
 function App() {
-  const [produktA, setProduktA] = useState(["Nokia 3310", "Nejvíc legendární mobilní telefon, na kterém můžete hrát snakea"]);
-  const [produktB, setProduktB] = useState(["iPhone 12 mini", "Velmi malý a výkoný telefon, který ohromí svým výkonem, ale baterií zas tak ne"]);
-  const [produktC, setProduktC] = useState(["Nothing Phone", "První telefon od nové firmy Nothing, Elnur si ho celkem chválí"]);
+  const [produktA, setProduktA] = useState([faker.commerce.productName(), faker.commerce.productDescription()]);
+  const [produktB, setProduktB] = useState([faker.commerce.productName(), faker.commerce.productDescription()]);
+  const [produktC, setProduktC] = useState([faker.commerce.productName(), faker.commerce.productDescription()]);
+
+  const [produktD, setProduktD] = useState([faker.commerce.productName(), faker.commerce.productDescription()]);
+  const [produktE, setProduktE] = useState([faker.commerce.productName(), faker.commerce.productDescription()]);
+  const [produktF, setProduktF] = useState([faker.commerce.productName(), faker.commerce.productDescription()]);
+
+  const [produktG, setProduktG] = useState([faker.commerce.productName(), faker.commerce.productDescription()]);
+  const [produktH, setProduktH] = useState([faker.commerce.productName(), faker.commerce.productDescription()]);
+  const [produktI, setProduktI] = useState([faker.commerce.productName(), faker.commerce.productDescription()]);
 
   return (
     <div>
       <Header content="ElektroShop"/>
       <Navbar />
       <ProductRow nazevA={produktA[0]} popisA={produktA[1]} nazevB={produktB[0]} popisB={produktB[1]} nazevC={produktC[0]} popisC={produktC[1]}/>
-      <ProductRow />
-      <ProductRow />
+      <ProductRow nazevA={produktD[0]} popisA={produktD[1]} nazevB={produktE[0]} popisB={produktE[1]} nazevC={produktF[0]} popisC={produktF[1]}/>
+      <ProductRow nazevA={produktG[0]} popisA={produktG[1]} nazevB={produktH[0]} popisB={produktH[1]} nazevC={produktI[0]} popisC={produktI[1]}/>
       <Navigation />
     </div>
   );
